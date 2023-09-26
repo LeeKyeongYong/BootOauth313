824cce99a42e8a3276c4f19fb838c869761022ee
이경용 <sleekydz86@naver.com>
2023-09-26 22:19:04 +0900

    비번을 닉네임으로
    비번을 닉네임으로

--- src/main/java/com/social/demo3/domain/service/MemberService.java
+++ src/main/java/com/social/demo3/domain/service/MemberService.java
@@ -33,7 +33,7 @@ public class MemberService {
         if (opMember.isPresent()) return opMember.get();
 
         // 소셜 로그인를 통한 가입시 비번은 없다.
-        return join(username, "", nickname, profileImgUrl); // 최초 로그인 시 딱 한번 실행
+        return join(username, username, nickname, profileImgUrl); // 최초 로그인 시 딱 한번 실행
     }
 
     public Optional<Member> findByUsername(String username) {
